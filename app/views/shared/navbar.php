01e89491d312e76209ed3b1434d8f179e7643c02
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-03-30 17:38:35 +0300

    AAPSE-33: Finished contact us form

--- app/views/shared/navbar.php
+++ app/views/shared/navbar.php
@@ -7,7 +7,7 @@
     <ul class="nav-links">
     <li><a href="/home">Home <i class="fas fa-home"></i></a></li>
     <li><a href="#">About <i class="fas fa-info-circle"></i></a></li>
-    <li><a href="#">Contact <i class="fas fa-envelope"></i></a></li>
+    <li><a href="/contact">Contact <i class="fas fa-envelope"></i></a></li>
     <li><a href="/login">Use Feature <i class="fas fa-cogs"></i></a></li>
 </ul>
 
