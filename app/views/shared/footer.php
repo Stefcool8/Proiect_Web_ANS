3f1dca0d0f6453c82ecaedcee75c7bfaf720b0dd
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-04-01 22:37:26 +0300

    AAPSE-28: Documented navbar + footer, improved both

--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -1,19 +1,19 @@
 <footer class="footer-container">
-  <div class="footer-item">
+  <div class="about-item">
     <h3><i class="fas fa-info-circle"></i> About Us</h3>
     <ul>
-      <li><a href="#">Tearm of Services</a></li>
+      <li><a href="#">Terms of Services</a></li>
       <li><a href="#">Privacy Policy</a></li>
       <li><a href="#">Sitemap</a></li>
     </ul>
   </div>
-    <div class="footer-item">
-      <h3><i class="fas fa-phone-alt"></i> Contact Us</h3>
-      <p><i class="fas fa-envelope"></i> Email: info@example.com</p>
-      <p><i class="fas fa-phone"></i> Phone: +1 (555) 123-4567</p>
-      <p><i class="fab fa-github"></i> <a href="#">GitHub</a></p>
-    </div>
-  <div class="social-icons">
+  <div class="contact-item">
+    <h3><i class="fas fa-phone-alt"></i> Contact Us</h3>
+    <p><i class="fas fa-envelope"></i> Email: info@example.com</p>
+    <p><i class="fas fa-phone"></i> Phone: +1 (555) 123-4567</p>
+    <p><i class="fab fa-github"></i><a href="#">GitHub</a></p>
+  </div>
+  <div class="social-item">
     <a href="#"><i class="fab fa-facebook-f"></i></a>
     <a href="#"><i class="fab fa-twitter"></i></a>
     <a href="#"><i class="fab fa-instagram"></i></a>
