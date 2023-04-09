9de1053807c79b720c760db728b5f19b29ca85e0
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-04-09 12:07:28 +0300

    AAPSE-28: Login form finished

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/navbar.css">
-    <link rel="stylesheet" href="./assets/css/footer.css">
-    <link rel="stylesheet" href="./assets/css/login.css">
-    <script src="./assets/js/navbar.js" defer></script>
-    <script src="./assets/js/footer.js" defer></script>
-    <title>Home Page</title>
-</head>
-<body>
 
-    <div class="parent">
-        <div class="div1"> 
-            <?php require_once '../app/views/shared/navbar.php'; ?>
-        </div>
-
-        <div class="main-content">
-            <div class="container">
-                <h2>Login Form</h2>
-                <form>
-                    <div class="input-group">
-                        <label for="username">Username</label>
+<div class="login">
+    <?php require_once '../app/views/shared/navbar.php'; ?>
+    <main class="main-content">
+        <div class="container">
+            <h2>Login Form</h2>
+            <form action="/login" method="post">
+                <div class="input-group">
+                    <label for="username">Username</label>
                         <input type="text" id="username" name="username" required>
                     </div>
                     <div class="input-group">
@@ -31,19 +14,13 @@
                         <input type="password" id="password" name="password" required>
                     </div>
                     <button type="submit">Log In</button>
-                </form>
-                <div class="signup-link">
-                    <p>Don't have an account? <a href="/signup">Sign up</a></p>
-                </div>
+            </form>
+            <div class="signup-link">
+                <p>Don't have an account? <a href="/signup">Sign up</a></p>
+                <p>Forgot your password? <a href="/password/reset">Reset password</a></p>
             </div>
         </div>
+    </main>
+    <?php require_once '../app/views/shared/footer.php'; ?>
+</div>
 
-        <div class="div3"> 
-            <?php require_once '../app/views/shared/footer.php'; ?>
-        </div>
-    </div>
- 
-
-
-</body>
-</html>
