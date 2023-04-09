0b2654eb6bca75cb61d8420864fae66d1dd2ef40
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-04-09 10:44:29 +0300

    AAPSE-28: Contact form finished

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/navbar.css">
-    <link rel="stylesheet" href="./assets/css/footer.css">
-    <link rel="stylesheet" href="./assets/css/about.css">
-    <script src="./assets/js/navbar.js" defer></script>
-    <script src="./assets/js/footer.js" defer></script>
-    <script src="./assets/js/about.js" defer></script>
-    <title>About us</title>
-</head>
-<body>
-
-
-        <div class="div1"> 
-            <?php require_once '../app/views/shared/navbar.php'; ?>
-        </div>
-
+<div class="about">
+    <?php require_once '../app/views/shared/navbar.php'; ?>
         <div class="main-content">
             <div class="show">
                 <!-- <img src="../assets/img/about.jpg" class="image"> -->
@@ -53,11 +34,5 @@
             </div>
         </div>
 
-        <div class="div3"> 
-            <?php require_once '../app/views/shared/footer.php'; ?>
-        </div>
-   
-
-</body>
-</html>
-
+    <?php require_once '../app/views/shared/footer.php'; ?>
+</div>
