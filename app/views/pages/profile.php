77a163921c6c91009eb017a317cd49ba716f7051
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-04-10 09:10:30 +0300

    main: last modification

--- app/views/pages/profile.php
+++ app/views/pages/profile.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/navbar.css">
-    <link rel="stylesheet" href="./assets/css/footer.css">
-    <link rel="stylesheet" href="./assets/css/profile.css">
-    <script src="./assets/js/navbar.js" defer></script>
-    <script src="./assets/js/footer.js" defer></script>
-    <title>User Profile</title>
-</head>
-<body>
 
     <div class="profile">
         <div class="header">
@@ -73,6 +59,3 @@
             <?php require_once '../app/views/shared/footer.php'; ?>
         </div>
     </div>
-
-</body>
-</html>
