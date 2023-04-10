7bc2990097dc3fe641411cbaa7bba66207c9614d
Stefcool8 <stefan.nastasiu8@gmail.com>
2023-04-10 06:41:08 +0300

    Added .js files for all buttons in Upload webpage

--- public/assets/js/upload.js
+++ public/assets/js/upload.js
@@ -1,37 +1,9 @@
 const pasteButton = document.getElementById("paste");
-const uploadButton = document.getElementById("upload");
+const dropButton = document.getElementById("drag-drop");
 const urlButton = document.getElementById("url");
 const tryButton = document.getElementById("try");
 
 const pasteForm = document.getElementById("paste-form");
-const uploadForm = document.getElementById("upload-form");
+const dropForm = document.getElementById("drag-drop-form");
 const urlForm = document.getElementById("url-form");
 const tryForm = document.getElementById("try-form");
-
-pasteButton.addEventListener("click", () => {
-    pasteForm.style.display = "block";
-    uploadForm.style.display = "none";
-    urlForm.style.display = "none";
-    tryForm.style.display = "none";
-});
-
-uploadButton.addEventListener("click", () => {
-    pasteForm.style.display = "none";
-    uploadForm.style.display = "block";
-    urlForm.style.display = "none";
-    tryForm.style.display = "none";
-});
-
-urlButton.addEventListener("click", () => {
-    pasteForm.style.display = "none";
-    uploadForm.style.display = "none";
-    urlForm.style.display = "block";
-    tryForm.style.display = "none";
-});
-
-tryButton.addEventListener("click", () => {
-    pasteForm.style.display = "none";
-    uploadForm.style.display = "none";
-    urlForm.style.display = "none";
-    tryForm.style.display = "block";
-});
