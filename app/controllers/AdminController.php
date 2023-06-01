dc0ddae00de25b59420fd0a6eef4798f409fe205
CobaschiAser <100200669+CobaschiAser@users.noreply.github.com>
2023-06-01 15:24:44 +0300

    Basic Admin Functionallity

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,204 @@
+<?php
+// DONE
+namespace App\Controllers;
+
+use App\Utils\ViewLoader;
+use App\Utils\ResponseHandler;
+use App\Utils\JWT;
+use App\Utils\Database;
+
+/**
+ * Controller for the Admin page.
+ * 
+ */
+class AdminController {
+
+    /**
+     * @OA\Get(
+     *     path="/api/admin",
+     *     summary="Retrieve admin page data",
+     *     operationId="getAdmin",
+     *     tags={"Admin"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="status_code",
+     *                 type="integer",
+     *                 example=200
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                     example="Admin Page"
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     * 
+     * @OA\Get(
+ *     path="/api/admin/users",
+ *     summary="Retrieve list of users",
+ *     operationId="getUsers",
+ *     tags={"Admin"},
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="status_code",
+ *                 type="integer",
+ *                 example=200
+ *             ),
+ *             @OA\Property(
+ *                 property="data",
+ *                 type="array",
+ *                 @OA\Items(
+ *                     type="object",
+ *                     @OA\Property(
+ *                         property="uuid",
+ *                         type="string",
+ *                         example="user-123"
+ *                     ),
+ *                     @OA\Property(
+ *                         property="name",
+ *                         type="string",
+ *                         example="John Doe"
+ *                     ),
+ *                     @OA\Property(
+ *                         property="email",
+ *                         type="string",
+ *                         example="johndoe@example.com"
+ *                     ),
+ *                     @OA\Property(
+ *                         property="username",
+ *                         type="string",
+ *                         example="johndoe"
+ *                     )
+ *                 )
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="Users not found",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="error",
+ *                 type="string",
+ *                 example="Users not found"
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Internal Server Error",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="error",
+ *                 type="string",
+ *                 example="Internal Server Error"
+ *             )
+ *         )
+ *     )
+ * )
+ */
+
+     public function getInfo() {
+        ResponseHandler::getResponseHandler()->sendResponse(200, [
+            'title' => 'Admin Page',
+        ]);
+    }
+/*
+    public function get() {
+        // get the token from the request header
+        $headers = apache_request_headers();
+
+        if (!isset($headers['Authorization'])) {
+            return ResponseHandler::getResponseHandler()->sendResponse(401, ['error' => 'Unauthorized']);
+        }
+
+        try {
+            $token = str_replace('Bearer ', '', $headers['Authorization']);
+            
+            // decode the token
+            $payload = JWT::getJWT()->decode($token);
+
+            // send the data
+            return ResponseHandler::getResponseHandler()->sendResponse(200, [
+                'data' => [
+                    'title' => 'Dashboard',
+                    'username' => $payload['username']
+                ]
+            ]);
+        } catch (\InvalidArgumentException $e) {
+            return ResponseHandler::getResponseHandler()->sendResponse(401, ['error' => 'Unauthorized']);
+        }
+    }
+*/
+
+public function getUsers(){
+    try {
+        $db = Database::getInstance();
+       // echo 'Salut';
+        
+        $users = $db->fetchAll("SELECT * FROM user ");
+
+        if (empty($users)) {
+            return ResponseHandler::getResponseHandler()->sendResponse(404, ['error' => 'Users not found']);
+        }
+
+        $userArray = [];
+        foreach ($users as $user) {
+            $userArray[] = [
+                'uuid' => $user['uuid'],
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'username' => $user['username']
+            ];
+        }
+        //echo $userArray;
+        //echo count($userArray);
+        /*foreach ($users as $user) {
+           
+                echo $user['uuid'];
+                echo $user['name'];
+                echo $user['email'];
+                echo $user['username'];
+        }
+        */
+        return ResponseHandler::getResponseHandler()->sendResponse(200, ['data' => $userArray]);
+
+/*
+return ResponseHandler::getResponseHandler()->sendResponse(200, [
+    [
+        'uuid' => 'user-123',
+        'name' => 'John Doe',
+        'email' => 'johndoe@example.com',
+        'username' => 'johndoe'
+    ],
+    [
+        'uuid' => 'user-456',
+        'name' => 'Jane Smith',
+        'email' => 'janesmith@example.com',
+        'username' => 'janesmith'
+    ]
+]);
+*/
+
+    } catch (\Exception $e) {
+        // Handle potential exception during database deletion
+        return ResponseHandler::getResponseHandler()->sendResponse(500, ["error" => "Internal Server Error"]);
+    }
+}
+
+}
---
+++ public/html/viewUsers.php
@@ -0,0 +1,118 @@
+<?php 
+    require_once __DIR__ . '/shared/general.php';
+    $result = fetch_data('admin/users', [
+        'data' => []
+    ]);
+    //var_dump($result);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+
+    <!-- css libraries -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- favicon -->
+    <link rel="icon" href="/public/assets/img/favicon.png">
+
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="stylesheet" href="/public/css/shared/navbar.css">
+    <link rel="stylesheet" href="/public/css/shared/footer.css">
+    <link rel="stylesheet" href="/public/css/viewUsers.css">
+</head>
+
+<body>
+    <?php require_once __DIR__ . '/shared/navbar.php'; ?>
+        <main class = "central-area">
+        <div class="page-name">
+            <p>
+                <?php
+                    echo "There are " . count($result['data']). " users registered.";
+                ?>
+            </p>
+        </div>
+        
+        <div class="project-area">
+
+
+        <?php
+        /*foreach ($result as $user) {
+            echo "UUID: " . $user['uuid'] . "<br>";
+            echo "Name: " . $user['name'] . "<br>";
+            echo "Email: " . $user['email'] . "<br>";
+            echo "Username: ".$user['username']."<br>";
+        }
+        */
+        ?>
+
+        
+        
+        <div class="user-list">
+            <?php foreach ($result['data'] as $user) : ?>
+                <div class="project project-1">
+                    <p><?php echo "Username: ".$user['username']; ?> <?php echo "|     UUID:".$user['uuid'];?></p>
+                    <div class ="button-area">
+                    <a href="/project/1" class="button">View Profile</a>
+                    <a href="/project/1/edit" class="button">Modify Profile</a>
+                    <a href="/project/<?=$user['uuid']?>/delete" class="button">Delete User</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+            
+        </div>
+        <!--<div class="project-area">
+                    
+            <div class = "project project-1">
+                <p>Project Name 1</p>
+                <div class ="button-area">
+                    <a href="/project/1" class="button">View</a>
+                    <a href="/project/1/edit" class="button">Modify</a>
+                    <a href="/project/1/delete" class="button">Delete</a>
+                </div>
+            </div>
+
+            <div class = "project project-2">
+                <p>Project Name 2</p>
+                <div class ="button-area">
+                    <a href="/project/2" class="button">View</a>
+                    <a href="/project/2/edit" class="button">Modify</a>
+                    <a href="/project/2/delete" class="button">Delete</a>
+                </div>
+            </div>
+                
+            <div class = "project project-3">
+                <p>Project Name 3</p>
+                <div class ="button-area">
+                    <a href="/project/3" class="button">View</a>
+                    <a href="/project/3/edit" class="button">Modify</a>
+                    <a href="/project/3/delete" class="button">Delete</a>
+                </div>
+            </div>
+
+            <div class = "project project-4">
+                <p>Project Name 4</p>
+                <div class ="button-area">
+                    <a href="/project/4" class="button">View</a>
+                    <a href="/project/4/edit" class="button">Modify</a>
+                    <a href="/project/4/delete" class="button">Delete</a>
+                </div>
+            </div>
+                    
+        </div>
+        !-->
+            
+
+    </main>
+
+    <?php require_once __DIR__ . '/shared/footer.php'; ?>
+
+    <script src="/public/js/viewUsers.js"></script>
+</body>
+
+</html>
