a353fd492c75661d00bb7c0ce5e03b713ddffebd
Stefcool8 <stefan.nastasiu8@gmail.com>
2023-06-16 02:25:47 +0300

    Stuck at CSVReader

--- tests/utils/CSVParserTest.php
+++ tests/utils/CSVParserTest.php
@@ -3,9 +3,176 @@
 namespace utils;
 
 use App\utils\CSVParser;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class CSVParserTest extends TestCase
 {
+    public function testGetCSVParser_ReturnsInstanceOfCSVParser()
+    {
+        // Arrange
+        $expected = CSVParser::class;
 
+        // Act
+        /** @var CSVParser|mixed $actual */
+        $actual = CSVParser::getCSVParser();
+
+        // Assert
+        $this->assertInstanceOf($expected, $actual);
+    }
+
+    public function testGetJsonRowFormat_ReturnsValidJsonString()
+    {
+        // Arrange
+        $csvFile = 'testFiles/test.csv';
+        $expected = '[
+            {
+                "\"name\",\"age\",\"city\"": "\"John\",\"30\",\"New York\""
+            },
+            {
+                "\"name\",\"age\",\"city\"": "\"Jane\",\"25\",\"London\""
+            }
+        ]';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        try {
+            /** @var string|mixed $result */
+            $result = $csvParser->getJsonRowFormat($csvFile);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        // Assert
+        $this->assertJsonStringEqualsJsonString($expected, $result);
+    }
+
+    public function testGetJsonColumnFormat_ReturnsValidJsonString()
+    {
+        // Arrange
+        $csvFile = 'testFiles/test.csv';
+        $expected = '{
+            "\"name\",\"age\",\"city\"": [
+                "\"John\",\"30\",\"New York\"",
+                "\"Jane\",\"25\",\"London\""
+            ]
+        }';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        try {
+            /** @var string|mixed $result */
+            $result = $csvParser->getJsonColumnFormat($csvFile);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        // Assert
+        $this->assertJsonStringEqualsJsonString($expected, $result);
+    }
+
+    public function testGetJsonFromColumn_ReturnsValidJsonString()
+    {
+        // Arrange
+        $csvFile = 'testFiles/test.csv';
+        $columnName = 'name';
+        $expected = '[
+            "John",
+            "Jane"
+        ]';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        try {
+            /** @var string|mixed $result */
+            $result = $csvParser->getJsonFromColumn($csvFile, $columnName);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetJsonFromRow_ReturnsValidJsonString()
+    {
+        // Arrange
+        $csvFile = 'test.csv';
+        $rowNumber = 1;
+        $expected = '{
+        "name": "Jane",
+        "age": "25",
+        "city": "London"
+    }';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        /** @var string|mixed $result */
+        $result = $csvParser->getJsonFromRow($csvFile, $rowNumber);
+
+        // Assert
+        $this->assertJsonStringEqualsJsonString($expected, $result);
+    }
+
+    public function testGetJsonRowFormat_ThrowsExceptionForInvalidCSVFile()
+    {
+        // Arrange
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error opening CSV file.');
+
+        $csvFile = 'nonexistent.csv';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        $csvParser->getJsonRowFormat($csvFile);
+    }
+
+    public function testGetJsonColumnFormat_ThrowsExceptionForInvalidCSVFile()
+    {
+        // Arrange
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error parsing CSV file: Error opening CSV file.');
+
+        $csvFile = 'nonexistent.csv';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        $csvParser->getJsonColumnFormat($csvFile);
+    }
+
+    public function testGetJsonFromColumn_ThrowsExceptionForInvalidCSVFile()
+    {
+        // Arrange
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error parsing CSV file: Error opening CSV file.');
+
+        $csvFile = 'nonexistent.csv';
+        $columnName = 'name';
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        $csvParser->getJsonFromColumn($csvFile, $columnName);
+    }
+
+    public function testGetJsonFromRow_ThrowsExceptionForInvalidCSVFile()
+    {
+        // Arrange
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Error parsing CSV file: Error opening CSV file.');
+
+        $csvFile = 'nonexistent.csv';
+        $rowNumber = 1;
+
+        $csvParser = CSVParser::getCSVParser();
+
+        // Act
+        $csvParser->getJsonFromRow($csvFile, $rowNumber);
+    }
 }
--- tests/utils/JWTTest.php
+++ tests/utils/JWTTest.php
@@ -3,9 +3,84 @@
 namespace utils;
 
 use App\utils\JWT;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class JWTTest extends TestCase
 {
+    public function testGetJWT_ReturnsInstanceOfJWT()
+    {
+        // Arrange
+        $expected = JWT::class;
 
+        // Act
+        /** @var JWT|mixed $actual */
+        $actual = JWT::getJWT();
+
+        // Assert
+        $this->assertInstanceOf($expected, $actual);
+    }
+
+    public function testEncodeReturnsValidJWT()
+    {
+        $payload = [
+            'sub' => '1234567890',
+            'name' => 'John Doe',
+            'iat' => 1516239022
+        ];
+
+        $expectedHeader = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+        $expectedPayload = 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ';
+        $expectedSignature = 'XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o';
+
+        $jwt = JWT::getJWT();
+
+        /** @var string|mixed $result */
+        $result = $jwt->encode($payload);
+        /** @var string|mixed $expectedJWT */
+        $expectedJWT = "$expectedHeader.$expectedPayload.$expectedSignature";
+
+        $this->assertEquals($expectedJWT, $result);
+    }
+
+    public function testDecodeReturnsValidPayload()
+    {
+        $header = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+        $payload = 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ';
+        $signature = 'XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o';
+
+        $jwt = "$header.$payload.$signature";
+
+        /** @var array|mixed $expectedPayload */
+        $expectedPayload = [
+            'sub' => '1234567890',
+            'name' => 'John Doe',
+            'iat' => 1516239022
+        ];
+
+        /** @var array|mixed $result */
+        $result = JWT::getJWT()->decode($jwt);
+
+        $this->assertEquals($expectedPayload, $result);
+    }
+
+    public function testDecodeThrowsExceptionForInvalidTokenFormat()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid token format');
+
+        $jwt = 'invalid_token';
+
+        JWT::getJWT()->decode($jwt);
+    }
+
+    public function testDecodeThrowsExceptionForInvalidSignature()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid signature');
+
+        $jwt = 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJzdWIiOiAiMTIzNDU2Nzg5MCIsICJuYW1lIjogIkpvaG4gRG9lIiwgImlhdCI6IDE1MTYyMzkwMjJ9.invalid_signature';
+
+        JWT::getJWT()->decode($jwt);
+    }
 }
