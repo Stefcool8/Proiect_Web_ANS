9c6cfa0a4e279a9151294e9a00ba7a0ac149f5fe
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-06-16 14:22:08 +0300

    AAPSE-41: Added view projects/project

--- public/html/user.php
+++ public/html/user.php
@@ -1,19 +1,17 @@
-<?php 
+<?php
 
-
-
-    $currentUrl = $_SERVER['REQUEST_URI'];
+$currentUrl = $_SERVER['REQUEST_URI'];
 
 // Parse the URL to extract the userID
-    $parts = explode('/', $currentUrl);
-    $userID = end($parts);
+$parts = explode('/', $currentUrl);
+$userID = end($parts);
 
-    require_once __DIR__ . '/shared/general.php';
+require_once __DIR__ . '/shared/general.php';
 
-    $urlAPI = 'user/'.$userID;
-    $result = fetch_data($urlAPI, [
-        'data' => []
-    ]);
+$urlAPI = 'user/' . $userID;
+$result = fetch_data($urlAPI, [
+    'data' => []
+]);
 
 ?>
 
@@ -33,116 +31,55 @@
     <title>User Profile</title>
 </head>
 <body>
-
 <div class="profile">
     <div class="header">
         <?php require_once __DIR__ . '/shared/navbar.php'; ?>
     </div>
-    <div class="main-content">
 
+    <div class="main-content">
         <div class="user-visual">
             <div class="user-banner">
                 <img src="/public/assets/img/banner.jpg" alt="User Banner">
             </div>
+
             <div class="user-picture-and-controls">
                 <div class="user-picture">
-
+                    <!-- User picture content goes here -->
                 </div>
+
                 <div class="user-name-and-email">
                     <h1> <?php echo $result['data']['username'] ?></h1>
                     <h5> <?php echo $result['data']['email'] ?> </h5>
                 </div>
+
                 <div class="controls">
-                    <button class="button">Cancel</button>
-                    <button class="button">Save</button>
+                    <button class="modify">Modify Account</button>
+                    <button class="delete">Delete Account</button>
+                    <button class="change-pass">Change Password</button>
                 </div>
             </div>
         </div>
 
         <div class="user-details">
-            <form>
-                <div class="input-group">
-                    <label for="firstname">First Name</label>
-                    <input type="text" name="firstname" id="firstname" placeholder=<?php echo $result['data']['name'] ?> required>
-                </div>
-                <div class="input-group">
-                    <label for="lastname">Last Name</label>
-                    <input type="text" name="lastname" id="lastname" placeholder=<?php echo $result['data']['name'] ?> required>
-                </div>
-                <div class="input-group">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" placeholder=<?php echo $result['data']['username'] ?> required>
-                </div>
-                <div class="input-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder=<?php echo $result['data']['email'] ?> required>
-                </div>
+            <div class="input-group">
+                <label>Full Name</label>
+                <p> <?php echo $result['data']['name'] ?> </p>
+            </div>
 
-                <?php //require_once '../app/views/shared/countries.php'; ?>
+            <div class="input-group">
+                <label>Email</label>
+                <p> <?php echo $result['data']['email'] ?> </p>
+            </div>
 
-                <div class="input-group">
-                    <label for="bio">Bio</label>
-                    <textarea name="bio" id="bio" cols="30" rows="10" placeholder="Let us know more about you"></textarea>
-                </div>
-            </form>
+            <div class="input-group">
+                <label>Username</label>
+                <p> <?php echo $result['data']['username'] ?> </p>
+            </div>
         </div>
-
     </div>
 
-    <div class="footer">
-        <?php require_once __DIR__ . '/shared/footer.php'; ?>
-    </div>
+    <?php require_once __DIR__ . '/shared/footer.php'; ?>
 </div>
 <script src="/public/js/user.js"></script>
-    <div class="profile">
-        <div class="header">
-            <?php require_once __DIR__ . '/shared/navbar.php'; ?>
-        </div>
-
-        <div class="main-content">
-            <div class="user-visual">
-                <div class="user-banner">
-                    <img src="/public/assets/img/banner.jpg" alt="User Banner">
-                </div>
-
-                <div class="user-picture-and-controls">
-                    <div class="user-picture">
-                        <!-- User picture content goes here -->
-                    </div>
-
-                    <div class="user-name-and-email">
-                        <h1> <?php echo $result['data']['username'] ?></h1>
-                        <h5> <?php echo $result['data']['email'] ?> </h5>
-                    </div>
-
-                    <div class="controls">
-                        <button class="modify">Modify Account</button>
-                        <button class="delete">Delete Account</button>
-                        <button class="change-pass">Change Password</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="user-details">
-                <div class="input-group">
-                    <label>Full Name</label>
-                    <p> <?php echo $result['data']['name'] ?> </p>
-                </div>
-
-                <div class="input-group">
-                    <label>Email</label>
-                    <p> <?php echo $result['data']['email'] ?> </p>
-                </div>
-
-                <div class="input-group">
-                    <label>Username</label>
-                    <p> <?php echo $result['data']['username'] ?> </p>
-                </div>
-            </div>
-        </div>
-
-        <?php require_once __DIR__ . '/shared/footer.php'; ?>
-    </div>
-    <script src="/public/js/user.js"></script>
 </body>
 </html>
