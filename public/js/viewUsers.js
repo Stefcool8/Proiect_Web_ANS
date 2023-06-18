ecd529a19e00a40db5c2f976ec59731f0bc7e62f
CobaschiAser <100200669+CobaschiAser@users.noreply.github.com>
2023-06-18 04:24:29 +0300

    Finalized Dashboard+ViewUsers+Pagination

--- public/js/viewUsers.js
+++ public/js/viewUsers.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', async() => {
     const token = localStorage.getItem('jwt');
+    const url = window.location.href;
+    const pageRegex = /page=(\d+)/;
+    const match = url.match(pageRegex);
+    const page = match ? match[1] : "1";
+    const pageSize = 4;
+    console.log(page);
     if (!token) {
         window.location.href = "/login";
     }
@@ -36,175 +42,230 @@ document.addEventListener('DOMContentLoaded', async() => {
         });
 
         console.log(response);
+        if(response.status === 404){
+            console.log("Nu am useri");
+            //const buttonPrevious = document.querySelector('.button-previous');
+            //const buttonNext = document.querySelector('.button-next');
+            //buttonPrevious.classList.add('hidden');
+            //buttonNext.classList.add('hidden');
+        }
+
         if (response.ok) {
 
             const result = await response.json();
             console.log(result);
             // Get the parent container where the user cards will be appended
+            const alertCard = document.createElement('div');
+            alertCard.classList.add('project');
             const introContainter = document.querySelector('.page-name');
-            const userCount = document.createElement('p');
             $count = result.data.data.length;
             //userCount.textContent = "Hello";
-            if ($count ==0){
-                userCount.textContent = "There is no user registered";
-            }else if($count == 1){
-                userCount.textContent = "There is only one user registered";
-            }else{
-                userCount.textContent = "There are " + $count+ " users registered";
+            if ($count == 0) {
+                alertCard.textContent = "There is no user registered";
+            } else if ($count == 1) {
+                alertCard.textContent = "There is only one user registered";
+            } else {
+                alertCard.textContent = "There are " + $count + " users registered";
             }
 
-            introContainter.appendChild(userCount);
+            introContainter.appendChild(alertCard);
 
 
             const userContainer = document.querySelector('.user-list');
 
-            // Iterate over the list of projects
-            result.data.data.forEach(user => {
-                // Create a new project card
-                const userCard = document.createElement('div');
-                userCard.classList.add('project');
-                userCard.classList.add(`project-${user.uuid}`); // Assign a unique class for this user
-
-                // Create the project name element
-                const userName = document.createElement('p');
-                userName.classList.add('project-name');
-                userName.textContent = user.username;
-
-                // Create the button area
-                const buttonArea = document.createElement('div');
-                buttonArea.classList.add('button-area');
-
-                // Create the view button
-                const viewButton = document.createElement('a');
-                viewButton.classList.add('button');
-                //viewButton.href = `/user/${user.uuid}`;
-                viewButton.textContent = 'View Profile';
-
-                viewButton.addEventListener('click', async (event) => {
-                    event.preventDefault();
-                    console.log('View Button clicked');
-                    console.log('UUID:${user.uuid}');
-
-
-                    try {
-                        const apiUrl = `/api/user/${user.uuid}`;
-                        const response = await fetch(apiUrl, {
-                            method: 'GET',
-                            headers: {
-                                Authorization: 'Bearer ' + token,
-                                'Content-Type': 'application/json',
-                            },
-                        });
-
-                        if (response.ok) {
-                            // The view was successful
-                            const URL = `/user/${user.uuid}`;
-                            window.location.href = URL;
-                        } else {
-                            // Handle the error
-                            console.error(result.message);
-                        }
-                    } catch (error) {
-                        // Handle any errors
-                        console.error(error);
+            try {
+                const apiURL = "/api/user/page/" + page;
+                const responseGetUserByPage = await fetch(apiURL, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: 'Bearer ' + token,
+                        'Content-Type': 'application/json',
                     }
-
                 });
 
-                // Create the view Projects button
-                const viewProjectsButton = document.createElement('a');
-                viewProjectsButton.classList.add('button');
-                //viewProjectsButton.href = `/viewUsers`;
-                viewProjectsButton.textContent = 'View Projects';
+                if (responseGetUserByPage.ok) {
+                    const resultGetUserByPage = await responseGetUserByPage.json();
+                    console.log(resultGetUserByPage);
 
-                viewProjectsButton.addEventListener('click', async (event) => {
-                    event.preventDefault();
-                    console.log('ViewProjects Button clicked');
-                    console.log('UUID:${user.uuid}');
+                    resultGetUserByPage.data.users.forEach(user => {
+                        // Create a new project card
+                        const userCard = document.createElement('div');
+                        userCard.classList.add('project');
+                        userCard.classList.add(`project-${user.uuid}`); // Assign a unique class for this user
+
+                        // Create the project name element
+                        const userName = document.createElement('p');
+                        userName.classList.add('project-name');
+                        userName.textContent = user.username;
+
+                        // Create the button area
+                        const buttonArea = document.createElement('div');
+                        buttonArea.classList.add('button-area');
+
+                        // Create the view button
+                        const viewButton = document.createElement('a');
+                        viewButton.classList.add('button');
+                        //viewButton.href = `/user/${user.uuid}`;
+                        viewButton.textContent = 'View Profile';
+
+                        viewButton.addEventListener('click', async (event) => {
+                            event.preventDefault();
+                            console.log('View Button clicked');
+                            console.log('UUID:${user.uuid}');
 
 
-                    try {
-                        const apiUrl = `/api/project/user/${user.uuid}/1`;
-                        const response = await fetch(apiUrl, {
-                            method: 'GET',
-                            headers: {
-                                Authorization: 'Bearer ' + token,
-                                'Content-Type': 'application/json',
-                            },
+                            try {
+                                const apiUrl = `/api/user/${user.uuid}`;
+                                const response = await fetch(apiUrl, {
+                                    method: 'GET',
+                                    headers: {
+                                        Authorization: 'Bearer ' + token,
+                                        'Content-Type': 'application/json',
+                                    },
+                                });
+
+                                if (response.ok) {
+                                    // The view was successful
+                                    const URL = `/user/${user.uuid}`;
+                                    window.location.href = URL;
+                                } else {
+                                    // Handle the error
+                                    console.error(result.message);
+                                }
+                            } catch (error) {
+                                // Handle any errors
+                                console.error(error);
+                            }
+
                         });
 
-                        if(response.status === 404){
-                            const URL = `/viewProjects/${user.uuid}?page=1`;
-                            window.location.href = URL;
-                        }
+                        // Create the view Projects button
+                        const viewProjectsButton = document.createElement('a');
+                        viewProjectsButton.classList.add('button');
+                        //viewProjectsButton.href = `/viewUsers`;
+                        viewProjectsButton.textContent = 'View Projects';
 
-                        if (response.ok) {
-                            // The view was successful
-                            const URL = `/viewProjects/${user.uuid}?page=1`;
-                            console.log(URL);
-                            window.location.href = URL;
-                        } else {
-                            // Handle the error
-                            console.error(result.message);
-                        }
-                    } catch (error) {
-                        // Handle any errors
-                        console.error(error);
-                    }
+                        viewProjectsButton.addEventListener('click', async (event) => {
+                            event.preventDefault();
+                            console.log('ViewProjects Button clicked');
+                            console.log('UUID:${user.uuid}');
 
-                });
 
-                // Create the delete button
-                const deleteButton = document.createElement('a');
-                deleteButton.classList.add('button');
-               // deleteButton.href = `/viewUsers`;
-                deleteButton.textContent = 'Delete';
+                            try {
+                                const apiUrl = `/api/project/user/${user.uuid}/1`;
+                                const response = await fetch(apiUrl, {
+                                    method: 'GET',
+                                    headers: {
+                                        Authorization: 'Bearer ' + token,
+                                        'Content-Type': 'application/json',
+                                    },
+                                });
 
-                deleteButton.addEventListener('click', async (event) => {
-                    event.preventDefault();
-                    console.log('Delete Button clicked');
-                    console.log('UUID:${user.uuid}');
+                                if (response.status === 404) {
+                                    const URL = `/viewProjects/${user.uuid}?page=1`;
+                                    window.location.href = URL;
+                                }
 
-                    try {
-                        const apiUrl = `/api/user/${user.uuid}`;
-                        const response = await fetch(apiUrl, {
-                            method: "DELETE",
-                            headers: {
-                                Authorization: 'Bearer ' + token,
-                                'Content-Type': 'application/json',
-                            },
-                            body: user.uuid,
+                                if (response.ok) {
+                                    // The view was successful
+                                    const URL = `/viewProjects/${user.uuid}?page=1`;
+                                    console.log(URL);
+                                    window.location.href = URL;
+                                } else {
+                                    // Handle the error
+                                    console.error(result.message);
+                                }
+                            } catch (error) {
+                                // Handle any errors
+                                console.error(error);
+                            }
+
                         });
 
-                        if (response.ok) {
-                            window.location.href = "/viewUsers";
-                        } else {
-                            // Handle the error
-                            console.error(result.message);
-                        }
-                    } catch (error) {
-                        // Handle any errors
-                        console.error(error);
+                        // Create the delete button
+                        const deleteButton = document.createElement('a');
+                        deleteButton.classList.add('button');
+                        // deleteButton.href = `/viewUsers`;
+                        deleteButton.textContent = 'Delete';
+
+                        deleteButton.addEventListener('click', async (event) => {
+                            event.preventDefault();
+                            console.log('Delete Button clicked');
+                            console.log('UUID:${user.uuid}');
+
+                            try {
+                                const apiUrl = `/api/user/${user.uuid}`;
+                                const response = await fetch(apiUrl, {
+                                    method: "DELETE",
+                                    headers: {
+                                        Authorization: 'Bearer ' + token,
+                                        'Content-Type': 'application/json',
+                                    },
+                                    body: user.uuid,
+                                });
+
+                                if (response.ok) {
+                                    window.location.href = "/viewUsers";
+                                } else {
+                                    // Handle the error
+                                    console.error(result.message);
+                                }
+                            } catch (error) {
+                                // Handle any errors
+                                console.error(error);
+                            }
+
+                        });
+                        // Append the project name and buttons to the project card
+                        userCard.appendChild(userName);
+                        buttonArea.appendChild(viewButton);
+                        buttonArea.appendChild(viewProjectsButton);
+                        buttonArea.appendChild(deleteButton);
+                        userCard.appendChild(buttonArea);
+
+                        // Append the project card to the parent container
+                        userContainer.appendChild(userCard);
+
+
+                    });
+
+                    const buttonPrevious = document.querySelector('.button-previous');
+                    const buttonNext = document.querySelector('.button-next');
+
+                    if (page === "1") {
+                        buttonPrevious.classList.add('hidden');
+                    } else {
+                        buttonPrevious.classList.remove('hidden');
                     }
 
-                });
-                // Append the project name and buttons to the project card
-                userCard.appendChild(userName);
-                buttonArea.appendChild(viewButton);
-                buttonArea.appendChild(viewProjectsButton);
-                buttonArea.appendChild(deleteButton);
-                userCard.appendChild(buttonArea);
+                    if (resultGetUserByPage.data.users.length < pageSize || $count <= pageSize*parseInt(page)) {
+                        buttonNext.classList.add('hidden');
+                    } else {
+                        buttonNext.classList.remove('hidden');
+                    }
 
-                // Append the project card to the parent container
-                userContainer.appendChild(userCard);
+                    buttonPrevious.addEventListener('click', async (event) => {
+                        event.preventDefault();
+                        window.location.href = `/viewUsers?page=${parseInt(page) - 1}`;
 
+                    });
 
-            });
+                    buttonNext.addEventListener('click', async (event) => {
+                        event.preventDefault();
+                        //console.log("Next button clicked");
+                        window.location.href = `/viewUsers?page=${parseInt(page) + 1}`;
+                    });
 
+                }
+                else {
+                    // Handle the error
+                    console.error(result.message);
+                }
+            } catch (error) {
+                // Handle any errors
+                console.error(error);
+            }
 
-        } else {
-            // Handle the error
-            console.error(result.message);
         }
     } catch (error) {
 
