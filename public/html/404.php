2a53f394a132187827861bffda8b7da87debcf0e
Martinescu Nicolae <martinescunicolaee3@gmail.com>
2023-06-23 20:54:27 +0300

    main: 404

--- public/html/404.php
+++ public/html/404.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>404 Not Found</title>
-        <link rel="stylesheet" href="/public/css/404.css">
-    </head>
-
-    <body>
-        <div class="container">
-            <div class="error-code">404</div>
-            <div class="error-text">Page Not Found</div>
-            <a href="/" class="home-btn">Go to Home</a>
-        </div>
-    </body>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <link rel="stylesheet" href="/public/css/404.css">
+</head>
+<body>
+<div class="container">
+    <div class="error-code">404</div>
+    <div class="error-text">Page Not Found</div>
+    <a href="/" class="home-btn">Go to Home</a>
+</div>
+</body>
 </html>
